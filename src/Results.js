2e0e633df9163ba66980b8d757b6c1f35a9bc2f6
mrsMacAlister <75312297+mrsMacAlister@users.noreply.github.com>
2022-04-29 13:35:59 +0200

    rendered the word meanings

--- src/Results.js
+++ src/Results.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Meaning from "./Meaning";
 
 export default function Results(props){
    
@@ -9,9 +10,17 @@ export default function Results(props){
 
     if (props.results){
         return (
-            <div className="Results">hello from Results</div>
+            <div className="Results">
+            <h2>{props.results.word}</h2>
+            {props.results.meanings.map(function(meaning, index) {
+                return (<div key={index}>
+                    <Meaning meaning={meaning} />
+                </div>
+                );
+            })
+            }
+            </div>
         )
-
     } else {
         return null;
     }
